230d1b0c5b90310843db8229d87f2697cfa5ec3b
unknown <simonastroimanova@hotmail.com>
2023-12-27 18:13:10 +0100

    first java program

---
+++ src/com/javapoint/Student.java
@@ -0,0 +1,17 @@
+//A Java class which is a fully encapsulated class
+//It has a private data member and getter and setter
+package com.javapoint;
+
+public class Student {
+//private data member
+	private String name;
+	// getter method for name
+	public String getName() {
+		return name;
+	}
+	//setter method for name
+	public void setName(String name) {
+		this.name=name;
+	}
+}
+	
